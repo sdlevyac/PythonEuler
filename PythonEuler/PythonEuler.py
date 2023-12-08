c33205d3d9335f3966b66333e195a24f6daddabc
Shaun Levy <sd.levy@outlook.com>
2023-12-08 15:15:22 +0000

    First 5 answered

--- PythonEuler/PythonEuler.py
+++ PythonEuler/PythonEuler.py
@@ -10,6 +10,10 @@ def problem02():
 def problem03():
     return max([i for i in range(1, int(np.sqrt(600851475143)) + 1) if 600851475143 % i == 0 and isPrime(i)])
 
-print(problem01())
-print(problem02())
-print(problem03())
+def problem04():
+    return max([product for product in [nums4[i % len(nums4)] * nums4[int(i / len(nums4))] for i in range(len(nums4)**2)] if isPalindrome(product)])
+
+def problem05():
+    return lcmList(list(range(1,21)))
+
+print(problem05())
--- PythonEuler/utils.py
+++ PythonEuler/utils.py
@@ -1,3 +1,4 @@
+import string
 import numpy as np
 
 def fibList(limit):
@@ -11,3 +12,31 @@ def isPrime(num):
         if num % i == 0:
             return False
     return True
+
+def isPalindrome(text):
+    if type(text) == int:
+        return str(text)[::-1] == str(text)
+    else:
+        return text[::-1] == text
+
+def isDivisible(a, d):
+    return a % d == 0
+
+def gcd(x, y):
+   while(y):
+       x, y = y, x % y
+   return x
+
+def lcm(x, y):
+   lcm = (x*y)//gcd(x,y)
+   return lcm
+
+def lcmList(numbers):
+    while len(numbers) != 1:
+        newnumbers = [lcm(numbers[0], numbers[1])]
+        newnumbers.extend(numbers[2:])
+        numbers = [n for n in newnumbers]
+
+    print(numbers[0])
+        
+nums4 = [i for i in range(100,1000)]
